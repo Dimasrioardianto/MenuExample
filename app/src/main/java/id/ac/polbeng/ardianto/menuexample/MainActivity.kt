8d9197353825a971e5db996ba4e039313eb1f446
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-15 13:10:24 +0700

    Untuk menampilkan pilihan menu

--- app/src/main/java/id/ac/polbeng/ardianto/menuexample/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/ardianto/menuexample/MainActivity.kt
@@ -1,20 +1,53 @@
 package id.ac.polbeng.ardianto.menuexample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import id.ac.polbeng.ardianto.menuexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.myTollbar)
         }
+    //menambahkan override fungsi oncreate
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //menambahkan isi dari pilihan menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuFile -> {
+                showMessage("File Menu")
+                return true
+            }
+            R.id.menuEdit -> {
+                showMessage("Edit Menu")
+                return true
+            }
+            R.id.menuHelp -> {
+                showMessage("Help Menu")
+                return true
+            }
+            R.id.menuExit -> {
+                showMessage("Exit")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showMessage(msg: String) {
+        Snackbar.make(binding.rootLayout, msg,
+            Snackbar.LENGTH_LONG).show()
     }
 }
